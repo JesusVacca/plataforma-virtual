7e207d58d1816129a68b3eae2a767b1880ab052e
Jesus Vacca <jesus.vacca99@gmail.com>
2024-03-09 15:25:15 -0500

    actualizando

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const hamburguesa = document.getElementById("hamburguesa");
 const cart = document.getElementById("cart");
+const listaBotonesAdd = document.getElementsByClassName("add-cart");
+const tbody = document.getElementById("tbody");
+
 
 
 cart.onclick = () =>{
     document.getElementById("carrito").classList.toggle("mostrar-carrito");
 }
+
+
+ 
