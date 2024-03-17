10ec376afebbedea1afbe8d9d230210c63817cae
Jesus Vacca <jesus.vacca99@gmail.com>
2024-03-17 13:45:15 -0500

    Subiendo archivos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+const cart = document.getElementById("cart");
+const cart_content = document.getElementById("cartContent");
+const closeCart = document.getElementById("closeCart");
+
+cart.onclick = () =>{
+    cart_content.classList.toggle("activeCartContent");
+    closeCart.onclick = () =>{
+        cart_content.classList.toggle("activeCartContent");
+    }
+}
+
+
+window.addEventListener("scroll",function(){
+    const boton = document.getElementById("return");
+    const alturaPagina = document.documentElement.scrollHeight;
+    const alturaVentana = window.innerHeight;
+    var porcentajeScroll = (window.scrollY / (alturaPagina - alturaVentana)) * 100;
+    if(porcentajeScroll >= 20){
+        boton.style.display="block";
+    }else boton.style.display="none";
+
+})
 const hamburguesa = document.getElementById("hamburguesa");
 console.log(hamburguesa)
 
